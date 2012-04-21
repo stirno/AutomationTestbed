56ff25bf9a3ebe32af286b987fe0c07ac74246d0
Brandon Stirnaman <stirno@gmail.com>
2012-04-21 17:23:25 -0500

    More loltests

---
+++ AutomationTestbed/Controllers/FormsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutomationTestbed.Controllers
+{
+    public class FormsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AutomationTestbed/Controllers/InteractiveController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutomationTestbed.Controllers
+{
+    public class InteractiveController : Controller
+    {
+        //
+        // GET: /Interactive/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
